1b6976bff464c1c19950656359d5c2116bebc1b6
karthikeyanmuthukumaranm <karthi.ac.kdp@gmail.com>
2025-09-07 23:02:09 +0530

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,10 @@
+class Vechicle:
+    def start(self):
+        print('vechicle starts')
+class Car(Vechicle):
+    def car_horns(slef):
+        print('car horns')
+
+c=Car()
+c.start()
+c.car_horns()
